69ce07bf5f4eb30cbfa2efa9e631b5c8cc88dafc
caicindy87 <caicindy87@gmail.com>
2020-03-26 09:49:18 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
 # Enter your procedural solution here!
+require 'pry'
+def collect_multiples(limit)
+    nums = (1...limit).to_a
+    # binding.pry
+    # puts 's'
+    # fives = nums.select {|num| num.remainder(5) == 0}
+    # threes = nums.select {|num| num.remainder(3) == 0}
+    # threes.concat(fives)
+
+    nums.select do |num|
+        if num.remainder(3) == 0
+            num
+        elsif num.remainder(5) == 0
+            num
+        end
+    end
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
