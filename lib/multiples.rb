00cebd9f18cd79f243f2cb8ab7c7cfc256d47f05
lalogaribo <garibo.eduardo90@gmail.com>
2020-03-26 09:42:51 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+
+def sum_multiples(limit)
+  i = 3
+  sum = 0
+  while i < limit
+    sum += i if (i % 3).zero? || (i % 5).zero?
+    i += 1
+  end
+  sum
+end
+
+def collect_multiples(mul)
+  i = 3
+  arr = []
+  while i < mul
+    arr << i if (i % 3).zero? || (i % 5).zero?
+    i += 1
+  end
+  arr
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def sum_multiples
+    i = 3
+    sum = 0
+    while i < num
+      sum += i if (i % 3).zero? || (i % 5).zero?
+      i += 1
+    end
+    sum
+  end
+
+  def collect_multiples
+    i = 3
+    arr = []
+    while i < num
+      arr << i if (i % 3).zero? || (i % 5).zero?
+      i += 1
+    end
+    arr
+  end
+end
